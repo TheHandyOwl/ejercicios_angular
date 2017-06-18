f2b83e203179e7ffad215608413365111ecf17f1
Carlos <thehandyowl.io@gmail.com>
2017-06-19 00:08:01 +0200

    Añadimos lista de contactos y hacemos enlaces de clases y estilo

--- Agenda/src/app/app.component.ts
+++ Agenda/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // En selector indicamos el elemento HTML donde se instanciará este componente
   selector: 'app-root',
+  // En templateUrl indicamos la ruta al template del componente. Podríamos escribirlo inline en la propiedad 'template'
   templateUrl: './app.component.html',
+  //La alternativa si picamos el código, cosa inmanejable
+  //template: "<h1>HOLA KEEPCODERS</h1>",
+  // En styleUrls indicamos las rutas de los documentos CSS que contienen los estilos que afectan a ESTE componente
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Keepcoding';
+// export class AppComponent {
+  // Vamos a implementar el hook, pero para ello tenemos que importar el OnInit del core para que funcione
+export class AppComponent implements OnInit {
+
+  // Como norma dejamos los constructores vacíos
+  constructor() {
+    //console.log('Dejame vacío y usa ngOnInit');
+    //console.log('Puede que no esté enlazado el template y la cagues!!!');
+    //console.log('Porque no puedes trabajar con el template si no está cargado');
+  }
+
+  contactos: string[];
+
+  // Como se sigue quejando vamos a ver qué falta para que se calle
+  // En el hook 'OnInit' inicializamos los datos del componente.
+  ngOnInit() {
+    // Mejor aquí que en el constructor
+    console.log('Estoy vivo!');
+
+    //contactos = [];
+    // Mejor usar esta forma
+    this.contactos = [
+      'Tim Cook',
+      'Bill Gates',
+      'Elon Musk',
+      'Steve Wozniak',
+      'Sundar Pichai'
+    ];
+
+  }
+
+  eliminarContacto(contacto: string): void {
+    /*
+    this.contactos = this.contactos.filter( (contactoAComprobar: string): boolean => {
+      return contactoAComprobar !== contacto;
+    });
+    */
+    let posicion = this.contactos.indexOf(contacto);
+    this.contactos.splice(posicion, 1);
+  }
+
+  // Ya que estamos implementamos también este
+  ngOnDestroy() {
+    console.log('Como no muere por ahora no veremos su comportamiento');
+  }
 }
---
+++ Agenda/src/app/lista-contactos/lista-contactos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'tho-lista-contactos',
+  templateUrl: './lista-contactos.component.html',
+  styleUrls: ['./lista-contactos.component.css']
+})
+export class ListaContactosComponent implements OnInit {
+
+  // Vamos a permitir que el padre nos pase datos
+  // Con el decorador 'Input' exponemos atributas para que puedan enñazar datos desde otros componentes
+  @Input() datos: string[];
+
+  // Al querer notificar vemos que no hay EventEmitter
+  // Con el decorador 'Output' exponemos eventos a otros componentes. Es necesario, además, que el atributo decorado sea de tipo EventEmitter<T>
+  @Output() clickEnEliminar = new EventEmitter<string>();
+
+  // Para emitir datos usamos la función 'emit' del 'EventEmitter'
+  notificarEliminacion(contactoParaBorrar: string): void {
+    // Notificamos al padre pero no tenemos EventEmitter
+    // Tras crear e importar vamos al tema
+    this.clickEnEliminar.emit(contactoParaBorrar);
+  }
+
+  esGatesWozniak(contacto: string): boolean {
+    return contacto.indexOf('Gates') > -1 || contacto.indexOf('Wozniak') > -1;
+  }
+
+  esCookMusk(contacto: string): string {
+    return contacto.indexOf('Cook') > -1 || contacto.indexOf('Musk') > -1 ? '32px' : '14px';
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
