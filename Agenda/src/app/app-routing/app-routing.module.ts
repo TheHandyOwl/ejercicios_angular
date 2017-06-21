25eb36ab268e216fb19c27f327e03f26225a8dda
Carlos <thehandyowl.io@gmail.com>
2017-06-21 22:34:12 +0200

    Nuestra agenda ahora es SPA

---
+++ Agenda/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MisContactosComponent } from '../mis-contactos/mis-contactos.component';
+import { NuevoContactoComponent } from '../nuevo-contacto/nuevo-contacto.component';
+
+// Definimos las rutas de la aplicación. Cada una de ellas tiene que tener un a propiedad 'path', que es donde indicamos la ruta a navegar, y otra propiedad 'component', que es el compoennte qe responde de la ruta.
+const routes: Routes = [
+  {
+    path: 'mis-contactos',
+    component: MisContactosComponent
+  },
+  {
+    path: 'nuevo-contacto',
+    component: NuevoContactoComponent
+  },
+  {
+    path: '**', // ** Cualquier cosa. PONER AL FINAL PORQUE EL ORDEN IMPORTA!!!
+    pathMatch: 'full', // Cómo se comporta la ruta: que matchee hasta la ruta, hasta las opciones, todo
+    redirectTo: '/mis-contactos'
+  }
+];
+
+// Con 'RouterModule.forRoot()' registramos rutas en el módulo
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+//export class AppRoutingRoutingModule { }
+export class AppRoutingModule { }
--- Agenda/src/app/app.module.ts
+++ Agenda/src/app/app.module.ts
@@ -2,25 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ListaContactosComponent } from './lista-contactos/lista-contactos.component';
 
 import { ContactosService } from './contactos.service';
 import { FormularioAltaComponent } from './formulario-alta/formulario-alta.component';
+import { MisContactosComponent } from './mis-contactos/mis-contactos.component';
+import { NuevoContactoComponent } from './nuevo-contacto/nuevo-contacto.component';
 
 @NgModule({
   // En 'declarations' indicamos todos los componentes, pipes y directivas de mi aplicación
   declarations: [
     AppComponent,
     ListaContactosComponent,
-    FormularioAltaComponent
+    FormularioAltaComponent,
+    MisContactosComponent,
+    NuevoContactoComponent
   ],
   // En imports indicamos módulos de los cuales dependemos
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   // En 'providers' indicamos los proveedores de todas aquellas piezas que sean susceptibles de ser inyectadas como dependencias
   providers: [
